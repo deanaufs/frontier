7879a87202dac67a9a04ac0d21a9d28d28005db8
Hoffman <Hoffman@aufs.io>
2022-03-09 17:15:01 +0800

    chain_spec

--- template/node/src/chain_spec.rs
+++ template/node/src/chain_spec.rs
@@ -93,7 +93,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 				// Initial PoA authorities
 				vec![
 					authority_keys_from_seed("Alice"),
-					authority_keys_from_seed("Bob"),
+					// authority_keys_from_seed("Bob"),
 					authority_keys_from_seed("Charlie"),
 					authority_keys_from_seed("Dave"),
 					authority_keys_from_seed("Eve"),

f538924662a1b8e682f37e53a6184bfe4bbaf33c
Hoffman <Hoffman@aufs.io>
2022-03-29 16:33:34 +0800

    config spec

--- scripts/gen_raw_spec.sh
+++ scripts/gen_raw_spec.sh
@@ -1,8 +1,8 @@
 # 生成默认的配置模板
-./target/debug/frontier-template-node build-spec --disable-default-bootnode --chain local > ./tmp/Spec.json
+./target/release/frontier-template-node build-spec --disable-default-bootnode --chain local > ./tmp/Spec.json
 
 # 根据模板，修改customSpec.json的aura, grandp内容，
 python3 ./scripts/replace_spec_author.py ./tmp/Spec.json ./tmp/TempSpec.json
 
 # 产生启动时可用的Raw配置文件
-./target/debug/frontier-template-node build-spec --chain=./tmp/TempSpec.json --raw --disable-default-bootnode > ./tmp/RawSpec.json
+./target/release/frontier-template-node build-spec --chain=./tmp/TempSpec.json --raw --disable-default-bootnode > ./tmp/RawSpec.json

64de9cddd731eaba38f4627096b519202c0a5285
Hoffman <Hoffman@aufs.io>
2022-03-31 16:43:23 +0800

    update pm2 scripts

--- scripts/pm2_gen.py
+++ scripts/pm2_gen.py
@@ -1,13 +1,13 @@
 
 bootnode_cmd = """# 启动节点{0}
->./tmp/{0}.log;\\
 ./target/{4}/frontier-template-node purge-chain --base-path ./tmp/{0} --chain local -y;\\
+> ./tmp/{0}.log;\\
 pm2 start -n {0} --log ./tmp/{0}.log \\
-"./target/{4}/frontier-template-node \\
+./target/{4}/frontier-template-node --\\
     --base-path ./tmp/{0} \\
-    --chain local \\
-    --{0} \\
+    --chain {5} \\
     --port {1} \\
+    --public-addr /ip4/{6}/tcp/{1} \\
     --rpc-port {2} \\
     --ws-port {3} \\
     --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \\
@@ -15,34 +15,38 @@ pm2 start -n {0} --log ./tmp/{0}.log \\
     --rpc-cors all \\
     --unsafe-rpc-external \\
     --unsafe-ws-external \\
-    --node-key 0000000000000000000000000000000000000000000000000000000000000001";
+    --node-key 0000000000000000000000000000000000000000000000000000000000000001;
 sleep 0.1s
 """
 
 node_cmd = """# 启动节点{0}
->./tmp/{0}.log;\\
 ./target/{4}/frontier-template-node purge-chain --base-path ./tmp/{0} --chain local -y;\\
+> ./tmp/{0}.log;\\
 pm2 start -n {0} --log ./tmp/{0}.log \\
-"./target/{4}/frontier-template-node \\
+./target/{4}/frontier-template-node --\\
     --base-path ./tmp/{0} \\
-    --chain local \\
-    --{0} \\
+    --chain {5} \\
     --port {1} \\
+    --public-addr /ip4/{6}/tcp/{1} \\
     --rpc-port {2} \\
     --ws-port {3} \\
     --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \\
-    --validator"; 
+    --validator; 
 sleep 0.1s 
 """
 
 PORT_NO, RPC_PORT_NO, WS_PORT_NO = 1601, 1851, 1801
-account_list = ["alice", "bob", "charlie", "dave", "eve", "ferdie"]
+account_list = ["C01", "C02", "C03", "C04", "C05"]
+chain_file = "./tmp/RawSpec.json"
+ip = "221.121.151.89"
+# account_list = ["alice", "bob", "charlie", "dave", "eve", "ferdie"]
+# chain_file = "local"
 mode="release"
 
 # for (i,  account) in account_list
 port, rpc_port, ws_port = PORT_NO, RPC_PORT_NO, WS_PORT_NO
 for (i, a) in enumerate(account_list):
     if i == 0:
-        print(bootnode_cmd.format(a, port+i, rpc_port+i, ws_port+i, mode))
+        print(bootnode_cmd.format(a, port+i, rpc_port+i, ws_port+i, mode, chain_file, ip))
     else:
-        print(node_cmd.format(a, port+i, rpc_port+i, ws_port+i, mode))
+        print(node_cmd.format(a, port+i, rpc_port+i, ws_port+i, mode, chain_file, ip))
